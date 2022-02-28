4e51380bfbb8767521902ffa3b6418bb00ace916
unknown <w.tyler1997@gmail.com>
2022-02-28 15:06:35 -0800

    API

--- icecream/src/App.js
+++ icecream/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Search from './search';
 import Searchbar from './searchbar';
+import icelogo from './logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src='icecream.png' alt='icecream' />
        <Search />
        <Searchbar />
        
---
+++ icecream/src/logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function icelogo() {
+    return(
+        <img src="..\public\icecream.png" alt="icecream logo" />
+    )
+}
+
+export default icelogo;
